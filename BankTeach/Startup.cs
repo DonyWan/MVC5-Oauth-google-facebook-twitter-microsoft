5057c5079161d2e1da93c5b6f4c54a6c2ee31a4e
donywan <donywan@outlook.com>
2018-03-27 17:15:29 +0800

    添加项目文件。

---
+++ BankTeach/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BankTeach.Startup))]
+namespace BankTeach
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
